cd4815109a2ff3df4258b893f41496a6f5ab0cf7
ceara <cearamahinay@gmail.com>
2025-08-22 17:56:04 -0600

    Complete exercises 12 and 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+
+
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = a;
+  if (a == 0) return 1;
+  for (let i = a; i > 1; i--) {
+    factorial = factorial * (i - 1);
+  }
+  return factorial;
 };
+
 
 // Do not edit below this line
 module.exports = {
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
 
+
+const palindromes = function (str) {
+    
+    let cleanStr = str.replace(/[^\w\s]|_/g, "");
+    cleanStr = cleanStr.replaceAll(" ", "").toUpperCase();
+
+    let strArray = cleanStr.split("");
+
+    let string = strArray.join("");
+    let reversed = strArray.reverse().join("");
+    
+    console.log(string + " " + reversed);
+
+    return (string === reversed);
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
