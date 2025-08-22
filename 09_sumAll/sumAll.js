722ff8ed416cd16c0a0cdfe652d491b416e2fbe5
ceara <cearamahinay@gmail.com>
2025-08-22 01:14:09 -0600

    Complete exercises 12-14

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
 
+    let sum = 0;
+    for (i = a; i <= b; i++) {
+        sum = sum + i;
+    }
+
+    for (arg of arguments) {
+        if (
+            arg < 0 ||
+            arg%1 != 0 ||
+            typeof arg !== "number"
+        ) {
+            return "ERROR";
+        }
+    }
+    return sum;
 };
+
+let sum = sumAll(1, 4); // returns the sum of 
+console.log(sum);
+// 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
