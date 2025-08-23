4847f3b2d22e0abb7e176ace26409cf203993084
ceara <cearamahinay@gmail.com>
2025-08-22 18:51:55 -0600

    Get the Titles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,37 @@
-const getTheTitles = function() {
+
+
+
+const getTheTitlesBad = function(booksArr) {
+    // take array and return array of titles
+    let titles = booksArr.reduce(
+        (titlesArr, current) => {
+            console.log(current);
+            titlesArr.push(current.title);
+            return titlesArr;
+        }, [], 
+    );
+    console.log(titles);
+    return titles;
 
 };
+
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
+}
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+let titles = getTheTitles(books);
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
